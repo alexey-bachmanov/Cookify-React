5c66e8dbfc4c101b75bd11fb6b5a07c7b8dcfb93
Alexey Bachmanov <alexbachmanov@gmail.com>
2023-06-08 14:47:07 -0400

    Vercel deployment attempt #2

--- stores/store.tsx
+++ stores/store.tsx
@@ -43,13 +43,14 @@ const UIContextProvider = function ({
         (pageNum - 1) * ITEMSPERPAGE
       }`
     );
-    console.log(response);
+    console.log('response', response);
     if (!response.ok) {
       // catch 404 errors etc
       console.error('Failed to fetch reipes');
       return [null, null];
     }
     const data = await response.json();
+    console.log('data', data);
     let newResults = data.results.map((result: any) => {
       return { id: result.id, title: result.title, image: result.image };
     });
