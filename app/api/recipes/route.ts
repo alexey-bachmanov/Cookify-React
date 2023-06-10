68f9f3a00f96038f8539e88f992b66d5c9eeda0d
Alexey Bachmanov <alexbachmanov@gmail.com>
2023-06-10 09:37:17 -0400

    Vercel deployment attempt #4

--- app/api/recipes/route.ts
+++ app/api/recipes/route.ts
@@ -9,7 +9,12 @@ export async function GET(req: Request, res: Response) {
 
   const response = await fetch(
     `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&${paramString}`,
-    { method: 'GET', headers: { 'Access-Control-Allow-Origin': '*' } }
+    {
+      method: 'GET',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+    }
   );
 
   // console.log(
